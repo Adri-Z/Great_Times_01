55ff50a130cae04caaf7e2b8dae940b37c72e037
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-10-04 16:08:57 -0500

    Creando Cuenta
    Intento 1

---
+++ Experimento/Examen/Parte 2/Cuenta.py
@@ -0,0 +1,25 @@
+#CreadoPor: AIZCH
+#Fecha: 04/10/2019
+#Asunto: Crear la lase cuenta para crear un ejemplo de Banco con POO
+
+class Cuenta:
+  
+  def __init__ (self,s,t,p):
+    self.saldo=s
+    self.tipo=t
+    self.propietario=p
+    
+  def __str__ (self):
+    return "El saldo es: " + str(self.saldo) +"\n"+ "El tipo es: " + str(self.tipo)+"\n"+ "El propietario es: " + str(self.propietario)
+  
+  def depositar(self,valor):
+    if valor>0:
+      self.saldo += valor
+    else:
+      print("Error el valor a depositar es incorrecto")
+  
+  def retirar(self,valor):
+    if valor >0 and valor < self.saldo:
+      self.saldo -= valor
+    else:
+      print("Error el valor a retirar es incorrecto o excede el saldo de la cuenta")
