b8e1c7910d451b3d05e51b24ebe6d2ea430f302f
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-10-09 19:56:36 -0500

    Se creaPruebas.py
    comentando un poco, apoyandome en el código que puso vic en el grupo

---
+++ Menu/menu03/Pruebas.py
@@ -0,0 +1,26 @@
+#CreadoPor: AIZCH
+#Fecha:9/10/2019
+#Asunto: Archivo de pruebas par el menu de ejemplo del Banco
+
+from MenuBanco import*
+class Pruebas:
+  pass
+  
+  objeto1menu = MenuBanco() #Coment1
+  print (objeto.bienvenida)
+  
+  """
+  objeto1=MenuBanco()
+  print (self.bienvenida)
+  """
+
+
+
+#Coment1:
+  #Si bienvenida1 fuera un método primero escribiríamos este renglón , e inmediatamente:
+  # objeto1menu.bienvenida1
+  
+  #   print(objeto1menu)
+    #como es un atributo lo que queremos mostrar en pantalla, tenemos que usar el __str__ para mostrar las características, en forma de cadena.
+  #Pero como se guardaron en variables, intentaré imprimirlas directo, así: print (objeto.bienvenida)
+  
