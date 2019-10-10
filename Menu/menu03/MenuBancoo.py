798eab807a6d72f5628abc2de437619586e2bcc4
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-10-09 19:40:04 -0500

    Se crea MenuBancoo.py
    primer ejercicio de menu

---
+++ Menu/menu03/MenuBancoo.py
@@ -0,0 +1,11 @@
+#CreadoPor: AIZCH
+#Fecha: 9 oct 2019
+#Asunto: Tarea del 7 de octubre
+
+class MenuBancoo:
+  def __init__ (self):
+    self.bienvenida = "Menú del Banco del ejemplo"
+  
+  def bienvenida(self):
+    print(self.bienvenida)
+    print("Ingresar nombre")
