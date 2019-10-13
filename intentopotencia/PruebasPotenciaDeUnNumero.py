d217f19e7ba82af090f2cc495c2f287c7a758f1e
Adri-Z <54725497+Adri-Z@users.noreply.github.com>
2019-10-13 15:20:00 -0500

    Puerbas de potenciainento 1

---
+++ intentopotencia/PruebasPotenciaDeUnNumero.py
@@ -0,0 +1,22 @@
+#CreadoPor: AIZCH
+#Fecha:13/10/2019
+#Asunto: Practica3 || En Python POO || 
+### Creando archivo de pruebas para la clase PotenciaDeUnNumero
+
+from PotenciaDeUnNUmero import *
+class Pruebas:
+  pass
+  print("Algo")
+
+  termino1_base_exponente=PotenciaDeUnNumero(2,3,0)
+  #termino2_base_exponente=PotenciaDeUnNumero(2,2,0)
+  
+ # termino1_base_exponente.metodo_1_operacion_cambia_signo_base(1)
+ # termino2_base_exponente.metodo_2_operacion_calcula_potencia_de_a_y_b(2,3,0)
+
+termino1_base_exponente.metodo_2_operacion_calcula_potencia_de_a_y_b(self,self.atributo_base,self.atributo_exponente,self.atributo_respuesta)
+  
+  print(termino1_base_exponente)
+ # print(termino2_base_exponente)
+
+ print(termino1_base_exponente.metodo_2_operacion_calcula_potencia_de_a_y_b(self,self.atributo_base,self.atributo_exponente,self.atributo_respuesta))
